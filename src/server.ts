72f8305604af378a9b283d86d183dc4b180eee24
Matheus Pelozatto Sampaio <411658@unoesc.lan>
2025-11-12 21:11:21 -0300

    Estrutura inicial

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from './app'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
