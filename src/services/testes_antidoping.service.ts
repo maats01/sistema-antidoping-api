18570a9c36b99c98ca4c4cb475aabd7b40cd04e6
Matheus <matheusp.sampaio@gmail.com>
2025-11-15 20:30:44 -0300

    Feature: Implementação da camada service de Testes

---
+++ src/services/testes_antidoping.service.ts
@@ -0,0 +1,52 @@
+import { prisma } from '../lib/prisma'
+import { Teste, Prisma } from '@prisma/client'
+
+class TestesAntidopingService {
+
+    async getAll(): Promise<Teste[]> {
+        return await prisma.teste.findMany();
+    }
+
+    async getById(id: number): Promise<Teste | null> {
+        return await prisma.teste.findFirst({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    async create(data: Prisma.TesteCreateInput): Promise<Teste> {
+        const teste: Teste = await prisma.teste.create({
+            data: data
+        });
+
+        return teste;
+    }
+
+    async update(id: number, data: Prisma.TesteUpdateInput): Promise<Teste> {
+        const teste_atualizado: Teste = await prisma.teste.update({
+            where: {
+                id: id
+            },
+            data: data
+        });
+
+        return teste_atualizado;
+    }
+
+    async delete(id: number): Promise<boolean> {
+        const teste = await this.getById(id);
+
+        if (teste === null) {
+            return false;
+        }
+
+        await prisma.teste.delete({
+            where: {
+                id: id
+            }
+        });
+
+        return true;
+    }
+}
